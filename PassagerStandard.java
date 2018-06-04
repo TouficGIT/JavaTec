26634ba255c6d4b6db3bccd4f98d60c7d9aa4936
TouficGIT <meffe.emilien@gmail.com>
2018-06-04 10:30:03 +0200

    first commit

---
+++ PassagerStandard.java
@@ -0,0 +1,39 @@
+//source sans la documentation produite par javadoc.
+
+class PassagerStandard {
+
+  public PassagerStandard(String nom, int destination) {
+
+  }
+
+  public String nom() {
+    return null;
+  }
+
+  public boolean estDehors() { 
+    return false;
+  }
+
+  public boolean estAssis() {
+    return false;
+  }
+
+  public boolean estDebout() {
+    return false;
+  }
+
+  public void changerEnDehors() {
+  }
+
+  public void changerEnAssis() {
+  }
+
+  public void changerEnDebout() {
+  }
+
+  public void monterDans(Autobus t) {
+  }
+
+  public void nouvelArret(Autobus bus, int numeroArret) {
+  }
+}
