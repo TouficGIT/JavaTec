8a73bb4cbf67795f5caf323f2800e40b7534299f
Corentin <boscher.crt@gmail.com>
2018-06-04 10:41:13 +0200

    Premier push

---
+++ Autobus.java
@@ -0,0 +1,32 @@
+//source sans la documentation produite par javadoc.
+
+class Autobus {
+  public Autobus(int assis, int debout){
+  }
+
+  public boolean aPlaceAssise() {
+    return false;
+  }
+
+  public boolean aPlaceDebout() {
+    return false;
+  }
+
+  public void monteeDemanderAssis(PassagerStandard p) {
+  }
+
+  public void monteeDemanderDebout(PassagerStandard p) {
+  }
+
+  public void allerArretSuivant() {
+  }
+
+  public void arretDemanderAssis(PassagerStandard p) {
+  }
+
+  public void arretDemanderDebout(PassagerStandard p) {
+  }
+
+  public void arretDemanderSortie(PassagerStandard p) {
+  }
+}
---
+++ PassagerStandard.java
@@ -0,0 +1,40 @@
+//source sans la documentation produite par javadoc.
+
+class PassagerStandard {
+  private String nomPassager;
+  private int destinationPassager;
+
+  public PassagerStandard(String nom, int destination){
+  }
+
+  public String nom() {
+    return null;
+  }
+
+  public boolean estDehors() { 
+    return false;
+  }
+
+  public boolean estAssis() {
+    return false;
+  }
+
+  public boolean estDebout() {
+    return false;
+  }
+
+  public void changerEnDehors() {
+  }
+
+  public void changerEnAssis() {
+  }
+
+  public void changerEnDebout() {
+  }
+
+  public void monterDans(Autobus t) {
+  }
+
+  public void nouvelArret(Autobus bus, int numeroArret) {
+  }
+}
