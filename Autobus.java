6a06871e352287b82a78df4be0c8b64cdf260d68
TouficGIT <meffe.emilien@gmail.com>
2018-06-04 12:12:50 +0200

    Modif autobus

--- Autobus.java
+++ Autobus.java
@@ -1,40 +1,80 @@
 //source sans la documentation produite par javadoc.
 
 class Autobus {
-<<<<<<< HEAD
-  public Autobus(int assis, int debout){
-=======
-  private int nbPlaceAssise;
-  private int nbPlaceDebout;
+
+  private Jauge placesAssises;
+  private Jauge placesDebout;
+  private int assis;
+  private int debout;
+  private int arret;
+  private final PassagerStandard[] passagers;
 
   public Autobus(int nbPlaceAssise, int nbPlaceDebout) {
-
->>>>>>> 26634ba255c6d4b6db3bccd4f98d60c7d9aa4936
+      arret = 0;
+      assis = 0;
+      debout = 0;
+      passagers = new PassagerStandard[NB_MAX];
+      placesAssises = new Jauge(nbPlaceAssise,assis);
+      placesDebout = new Jauge(nbPlaceDebout,debout);
   }
 
   public boolean aPlaceAssise() {
-    return false;
+
+    if (placesAssises.estVert()) {
+        return true;
+    }
+    else
+        return false;
   }
 
   public boolean aPlaceDebout() {
-    return false;
+      if (placesDebout.estVert()) {
+          return true;
+      }
+      else
+          return false;
   }
 
   public void monteeDemanderAssis(PassagerStandard p) {
+      assis++;
+
+
   }
 
   public void monteeDemanderDebout(PassagerStandard p) {
+      assis++;
+
   }
 
   public void allerArretSuivant() {
+      arret++;
+      passagers;
+
   }
 
   public void arretDemanderAssis(PassagerStandard p) {
+      assis--;
+      debout++;
+      p.estDebout() = true;
+      p.estAssis() = false;
   }
 
   public void arretDemanderDebout(PassagerStandard p) {
+      assis++;
+      debout--;
+      p.estDebout() = false;
+      p.estAssis() = true;
   }
 
   public void arretDemanderSortie(PassagerStandard p) {
+      if (p.estDebout(p)) {
+          debout--;
+      }
+
+      if (p.estAssis(p)) {
+          assis-;
+      }
+      p.estDebout(p) = false;
+      p.estAssis(p) = false;
   }
 }
