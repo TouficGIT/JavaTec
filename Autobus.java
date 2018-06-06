b50f94d404ccb28254ae12cfae71dbd0e2ba6861
TouficGIT <meffe.emilien@gmail.com>
2018-06-06 08:50:02 +0200

    Fin modif classe Autobus

--- Autobus.java
+++ Autobus.java
@@ -6,14 +6,15 @@ class Autobus {
   private Jauge placesDebout;
   private int assis;
   private int debout;
-  private int arret;
-  private final PassagerStandard[] passagers;
+  private int arret = 0;
+  private int nb_passagers = 0;
+  private PassagerStandard[] passager_standard;
 
   public Autobus(int nbPlaceAssise, int nbPlaceDebout) {
       arret = 0;
       assis = 0;
       debout = 0;
-      passagers = new PassagerStandard[NB_MAX];
+      passager_standard = new PassagerStandard[nbPlaceAssise+nbPlaceDebout];
       placesAssises = new Jauge(nbPlaceAssise,assis);
       placesDebout = new Jauge(nbPlaceDebout,debout);
   }
@@ -36,45 +37,83 @@ class Autobus {
   }
 
   public void monteeDemanderAssis(PassagerStandard p) {
-      assis++;
-
-
+      // Si le passager n'est pas dehors ou qu'il n'y a pas de place assise
+      if (!p.estDehors() || !aPlaceAssise()) {
+        System.out.println("Demande de montée assise impossible");
+        return;
+      }
+      placesAssises.incrementer(); // on incrémente le nombre de places assises prisent
+      p.changerEnAssis();
+      passager_standard[nb_passagers] = p;
+      nb_passagers++;
   }
 
   public void monteeDemanderDebout(PassagerStandard p) {
-      assis++;
+      // Si le passager n'est pas dehors ou qu'il n'y a pas de place assise
+      if (!p.estDehors() || !aPlaceDebout()) {
+          System.out.println("Demande de montée debout impossible");
+          return;
+      }
+      placesAssises.incrementer(); // on incrémente le nombre de places assises prisent
+      p.changerEnAssis();
+      passager_standard[nb_passagers] = p;
+      nb_passagers++;
 
   }
 
   public void allerArretSuivant() {
-      arret++;
-      passagers;
 
+      arret++;
+
+      for (int i = 0; i < passager_standard.length; i++) {
+          if (passager_standard[i] != null)
+              passager_standard[i].nouvelArret(this, arret);
+      }
   }
 
+
   public void arretDemanderAssis(PassagerStandard p) {
-      assis--;
-      debout++;
-      p.estDebout() = true;
-      p.estAssis() = false;
+      // Si le passager n'est pas dehors ou qu'il n'y a pas de place assise
+      if (!p.estDehors() || aPlaceAssise()) {
+          System.out.println("Demande de place assise, à l'arret, impossible");
+          return;
+      }
+      placesDebout.decrementer();
+      placesAssises.incrementer(); // on incrémente le nombre de places assises prisent
+      p.changerEnAssis();
   }
 
   public void arretDemanderDebout(PassagerStandard p) {
-      assis++;
-      debout--;
-      p.estDebout() = false;
-      p.estAssis() = true;
+      // Si le passager n'est pas dehors ou qu'il n'y a pas de place assise
+      if (!p.estDehors() || aPlaceDebout()) {
+          System.out.println("Demande de place debout, à l'arret, impossible");
+          return;
+      }
+      placesAssises.decrementer();
+      placesDebout.incrementer(); // on incrémente le nombre de places assises prisent
+      p.changerEnDebout();
   }
 
   public void arretDemanderSortie(PassagerStandard p) {
       if (p.estDebout(p)) {
-          debout--;
+          placesDebout.decrementer();
       }
 
-      if (p.estAssis(p)) {
-          assis-;
+      else if (p.estAssis(p)) {
+          placesAssises.decrementer();
       }
-      p.estDebout(p) = false;
-      p.estAssis(p) = false;
+
+      else {
+          return;
+      }
+
+      for (int i = 0; i < passager_standard.length; i++) {
+          if (passager_standard[i] == p)
+              passager_standard[i] = null;
+      }
+
+      p.changerEnDehors();
+      nb_passagers--;
+
   }
 }
