98e9346ac8e6f0dda5794c8337228a7fc36fce7c
corentin-boscher <boscher.crt@gmail.com>
2018-06-06 08:51:36 +0200

    Fin modif classe PassagerStandard

--- PassagerStandard.java
+++ PassagerStandard.java
@@ -1,46 +1,59 @@
 //source sans la documentation produite par javadoc.
 
 class PassagerStandard {
-<<<<<<< HEAD
-  private String nomPassager;
-  private int destinationPassager;
+  private final String nomPassager;
+  private final int destinationPassager;
+  private Position positionPassager;
 
   public PassagerStandard(String nom, int destination){
-=======
-
-  public PassagerStandard(String nom, int destination) {
-
->>>>>>> 26634ba255c6d4b6db3bccd4f98d60c7d9aa4936
+    this.nomPassager=nom;
+    this.destinationPassager=destination;
+    positionPassager = new Position();
   }
 
   public String nom() {
-    return null;
+    return this.nomPassager;
   }
 
   public boolean estDehors() { 
-    return false;
+    return positionPassager.estDehors() ;
   }
 
   public boolean estAssis() {
-    return false;
+    return positionPassager.estAssis();
   }
 
   public boolean estDebout() {
-    return false;
+    return positionPassager.estDebout();
   }
 
   public void changerEnDehors() {
+    positionPassager = positionPassager.dehors();
   }
 
   public void changerEnAssis() {
+    positionPassager = positionPassager.assis();
   }
 
   public void changerEnDebout() {
+    positionPassager = positionPassager.debout();
   }
 
   public void monterDans(Autobus t) {
+    if (t.aPlaceAssise()){
+      t.monteeDemanderAssis(this);
+    }
+    else if (t.aPlaceDebout()){
+      t.monteeDemanderDebout(this);
+    }
   }
 
   public void nouvelArret(Autobus bus, int numeroArret) {
+    if (destinationPassager <= numeroArret){
+      bus.arretDemanderSortie(this);
+    }
+    //else if (estDebout() && bus.aPlaceAssise()){
+    //  bus.arretDemanderAssis(this);
+    //}
   }
 }
